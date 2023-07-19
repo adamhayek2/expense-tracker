a0f1cb77b315ae80c8c59100c7654f1942ec4824
adamhayek2002 <YOUR_EMAIL>
2023-07-19 14:36:35 +0300

    frontend <> adding delete row function

--- resources/js/jquery.js
+++ resources/js/jquery.js
@@ -5,26 +5,39 @@
 function singleExpenseElement(name, price){
     return `<tr>
                 <td class = "single-expense-Item">${name}</td>
-                <td class = "single-expense-price">${price}</td>
+                <td class = "single-expense-price">${price}$</td>
+                <td>
+                    <button class="delete-button">Delete</button>
+                </td>
             </tr>    `;
 }
 
 function addExpense() {
-    const nameInput = $("#expense-name").val();
-        const priceInput = $("#expense-price").val();
+    const nameInput = $("#expense-name").val().trim();
+    const priceInput = $("#expense-price").val().trim();
 
-        // Create a new row with the provided name and price
-        const newRow = singleExpenseElement(nameInput, priceInput);
+    // Validation if the input is empty
+    if (nameInput === "" || priceInput === "") return;
 
-        // Append the new row to the table
-        $("#expenses-table").append(newRow);
+    // Create a new row with the provided name and price
+    const newRow = singleExpenseElement(nameInput, priceInput);
 
-        // Clear the input fields after adding the expense
-        $("#expense-name").val("");
-        $("#expense-price").val("");
-        console.log("nice")
+    // Append the new row to the table
+    $("#expenses-table").append(newRow);
+
+    // Clear the input fields after adding the expense
+    $("#expense-name").val("");
+    $("#expense-price").val("");
 }
+function deleteExpenseRow(row) {
+    row.remove();
+  }
 $(document).ready(function () {
     const addButton = $("#add-button");
     addButton.click(addExpense);
+    
+    $(document).on("click", ".delete-button", function () {
+        const row = $(this).closest("tr");
+        deleteExpenseRow(row);
+      });
 });
